f26d3cf94fbf776d4b330c5f1244d24a8af985c3
Moses Munialo <munialom@gmail.com>
2022-03-16 11:27:37 +0300

    added using ListIterator

--- Country.java
+++ Country.java
@@ -1,31 +1,36 @@
 import java.util.List;
+import java.util.ListIterator;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
 
 public class Country {
 
-    //create a list of countries based on java collection array list
-    //we use traditional for loop to print the elements of the arrays
+    // create a list of countries based on java collection array list
+    // we use traditional for loop to print the elements of the arrays
 
+    static List<String> countriesList = List.of(
+            "USA", "China", "UK", "Canada", "Russia", "India", "Australia");
 
-    static List<String> countriesList = 
-    List.of(
-     "USA","China","UK","Canada","Russia","India","Australia"
-   );
+    static List<String> countries = List.of(
+            "KENYA", "UGANADA", "SOUTH AFRICA", "MALAWI", "EGYPT", "TUNISIA", "Australia");
 
-   static List<String> countries = 
-   List.of(
-    "KENYA","UGANADA","SOUTH AFRICA","MALAWI","EGYPT","TUNISIA","Australia"
-  );
-  
     public static void main(String[] args) {
-        
-        for(int i = 0; i < countriesList.size(); i++) {
+        // using traditional for loop
+        for (int i = 0; i < countriesList.size(); i++) {
             System.out.println(countriesList.get(i));
-         }
+        }
 
-
-         for(String country : countries) {
+        // using enhanced for loop
+        for (String country : countries) {
             System.out.println(country);
         }
+// using list iterator 
+ListIterator<String> itr= countriesList.listIterator();
+while (itr.hasNext()) {
+    String country = itr.next();
+    System.out.println(country);
+}
+      
     }
-    
+
 }
